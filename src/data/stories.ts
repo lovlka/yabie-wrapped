360207f6d2ae101ad63eb2bafe344b8657adea09
Victor Stodell <victor@stodell.se>
2023-12-13 16:02:19 +0100

    Replace story images

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -1,15 +1,7 @@
-import story1 from "@app/assets/stories/story1.jpg"
-import story2 from "@app/assets/stories/story2.jpg"
-import story3 from "@app/assets/stories/story3.jpg"
-import story4 from "@app/assets/stories/story4.jpg"
-import elder1 from "@app/assets/stories/elder1.jpg"
-import elder2 from "@app/assets/stories/elder2.jpg"
-import elder3 from "@app/assets/stories/elder3.jpg"
-import vacation1 from "@app/assets/stories/vacation1.jpg"
-import vacation2 from "@app/assets/stories/vacation2.jpg"
-import vacation3 from "@app/assets/stories/vacation3.jpg"
-import vacation4 from "@app/assets/stories/vacation4.jpg"
-import vacation5 from "@app/assets/stories/vacation5.jpg"
+import story1 from "@app/assets/stories/story1.webp"
+import story2 from "@app/assets/stories/story2.webp"
+import story3 from "@app/assets/stories/story3.webp"
+import story4 from "@app/assets/stories/story4.webp"
 
 export const STORIES = [
   {
@@ -19,15 +11,7 @@ export const STORIES = [
       story1,
       story2,
       story3,
-      story4,
-      elder1,
-      elder2,
-      elder3,
-      vacation1,
-      vacation2,
-      vacation3,
-      vacation4,
-      vacation5
+      story4
     ]
   }
 ]
